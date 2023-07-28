1970c03763a281fa119f5e81c3e5556c281a943b
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-07-29 02:58:12 +0800

    add favicon - undone

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { nanumMyeongjo, barlowSemiCondensed, playFairDisplay } from "./font.js";
+import Head from "next/head";
 
 export const metadata = {
     title: "Xiao Yun",
@@ -10,6 +11,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <Head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+            </Head>
+
+            {/*<link rel="icon" href="/favicon.ico" sizes="any" />*/}
             <body
                 className={`${nanumMyeongjo.variable} ${barlowSemiCondensed.variable} ${playFairDisplay.variable}`}
                 suppressHydrationWarning={true}
