a258fa7328515f3d5e9f80482d26eaee537f3a53
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-07-29 01:36:28 +0800

    add use effect for mobile

--- src/components/scrollTop/ScrollTop.js
+++ src/components/scrollTop/ScrollTop.js
@@ -22,11 +22,36 @@ export default function ScrollTop() {
         });
     }
 
+    // useEffect(() => {
+    //     // Add a listener for changes to the screen size
+    //     const mediaQuery = window.matchMedia("(max-width: 500px)");
+    //
+    //     // Set the initial value of the `isMobile` state variable
+    //     setIsMobile(mediaQuery.matches);
+    //
+    //     // Define a callback function to handle changes to the media query
+    //     const handleMediaQueryChange = (event) => {
+    //         setIsMobile(event.matches);
+    //     };
+    //
+    //     // Add the callback function as a listener for changes to the media query
+    //     mediaQuery.addEventListener("change", handleMediaQueryChange);
+    //
+    //     // Remove the listener when the component is unmounted
+    //     return () => {
+    //         mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    //     };
+    // }, []);
+
     useEffect(() => {
         if (typeof window !== "undefined") {
             // browser code
             window.addEventListener("scroll", handleVisible);
         }
+
+        return () => {
+            window.removeEventListener("scroll", handleVisible);
+        };
     }, []);
 
     return (
