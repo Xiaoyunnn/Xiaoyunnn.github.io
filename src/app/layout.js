23914f96c29515912e66a4214d368dccad15d22c
Wu Xiaoyun <xiaoyun.wu00@gmail.com>
2023-07-29 03:03:47 +0800

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,28 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <Head>
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/favicon/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/favicon/site.webmanifest" />
-            </Head>
-
-            {/*<link rel="icon" href="/favicon.ico" sizes="any" />*/}
             <body
                 className={`${nanumMyeongjo.variable} ${barlowSemiCondensed.variable} ${playFairDisplay.variable}`}
                 suppressHydrationWarning={true}
